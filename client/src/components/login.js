1cb5e3d26c6e7f10bc84fb73ac4dabd4e546f884
chi-wei-lien <lien2@purdue.edu>
2022-01-21 21:59:42 -0500

    login system done

--- client/src/components/login.js
+++ client/src/components/login.js
@@ -1,6 +1,24 @@
+import socialMediaAuth from "../service/auth";
+import { googleProvider } from "../service/auth-method";
+
 const Login = () => {
+  //login function
+  const login = async (provider) => {
+    const res = await socialMediaAuth(provider);
+    if (res.email) {
+      const user = {
+        email: res.email
+      }
+      console.log(user);
+    }
+  }
   return (
-    <h>hi this is cool</h>
+    <>
+      <h1>hi</h1>
+      <button
+        onClick={() => login(googleProvider)}
+      > google login</button>
+    </>
   )
 }
 
