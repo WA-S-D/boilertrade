e11843331db218a5f7ad92efb0d199f01804386f
chi-wei-lien <lien2@purdue.edu>
2022-01-21 20:43:02 -0500

    adding component folder

---
+++ client/config/firebase-config.js
@@ -0,0 +1,15 @@
+import firebase from 'firebase';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBP1Ij1SeJmEAvyju5UIdbU9ZtlA_s7d68",
+  authDomain: "boilertrade-ad136.firebaseapp.com",
+  projectId: "boilertrade-ad136",
+  storageBucket: "boilertrade-ad136.appspot.com",
+  messagingSenderId: "614415855825",
+  appId: "1:614415855825:web:632b6ee50ca815b8e415f0",
+  measurementId: "G-07TTRRGNMQ"
+};
+
+firebase.initializeApp(firebaseConfig);
+
+export default firebase;
