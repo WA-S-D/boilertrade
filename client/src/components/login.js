7f6ce760e71490318bb194fd0f117c2395dfc4e0
chi-wei-lien <lien2@purdue.edu>
2022-01-22 12:12:12 -0500

    remove body tag

--- client/src/components/login.js
+++ client/src/components/login.js
@@ -57,30 +57,28 @@ const Login = () => {
   }
 
   return (
-    <body>
-      <Container>
-        <Row>
-          <Col className = "brand">BoilerTrade</Col>
-        </Row>
-        <Row>
-          <Col>
-            <img src={"./images/login-photo.png"} className = "left-image"/>
-          </Col>
-          <Col>
-            <div className = "login-text">
-              <Stack gap={1} className="col-md-5 mx-auto">
-                <div className= "welcome-text">Welcome Back!</div>
-                <div className= "signup-text">Login or Sign Up to continue</div>
-                <Button variant="outline-light" className = "button" onClick={login}>
-                  <img src={"./images/google-logo.png"} className = "google"/>
-                  <div className="button-text">Sign in with Google</div>
-                </Button>
-              </Stack>
-            </div>
-          </Col>
-        </Row>
-      </Container>
-      </body>
+    <Container>
+      <Row>
+        <Col className = "brand">BoilerTrade</Col>
+      </Row>
+      <Row>
+        <Col>
+          <img src={"./images/login-photo.png"} className = "left-image"/>
+        </Col>
+        <Col>
+          <div className = "login-text">
+            <Stack gap={1} className="col-md-5 mx-auto">
+              <div className= "welcome-text">Welcome Back!</div>
+              <div className= "signup-text">Login or Sign Up to continue</div>
+              <Button variant="outline-light" className = "button">
+                <img src={"./images/google-logo.png"} className = "google"/>
+                <div className="button-text">Sign in with Google</div>
+              </Button>
+            </Stack>
+          </div>
+        </Col>
+      </Row>
+    </Container>
   )
 }
 
