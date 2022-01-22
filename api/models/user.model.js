745de83435900190ce9c60322a9b2884e07669fd
Anthony Baumann <anthony.baumann@icloud.com>
2022-01-21 21:59:27 -0500

    added model for user

---
+++ api/models/user.model.js
@@ -0,0 +1,25 @@
+const mongoose  = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+const userSchema  = new Schema({
+    username: {
+        type: String,
+        required: true,
+        trim: true,
+        unique: true,
+        minlength: 3
+    },
+    password: {
+        type: String,
+        required: true,
+        trime: true,
+        minlength: 3
+    }
+}, {
+    timestamps: true,
+});
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
