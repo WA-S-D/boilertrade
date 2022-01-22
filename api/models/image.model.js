2d769797f2a2a48d49a3b6e923e1bff8de99dc95
Anthony Baumann <anthony.baumann@icloud.com>
2022-01-22 12:11:28 -0500

    photo and their name/description uploading works

--- api/models/image.model.js
+++ api/models/image.model.js
@@ -3,7 +3,6 @@ var mongoose = require('mongoose');
 const Schema = mongoose.Schema;
   
 var imageSchema = new Schema({
-    email: String,
     name: String,
     desc: String,
     img:
--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,69 @@
 import logo from './logo.svg';
 import './App.css';
+import useState from 'react-hook-use-state';
+import axios from 'axios';
+import react, { Component } from 'react';
 
-function App() {
+class App extends Component {
+
+  constructor(props) {
+    super(props);
+      this.state = {
+        selectedFile: null,
+        desc: null,
+        name: null
+      }
+  }
+
+  onChangeHandler=event=> {
+    this.setState({
+      selectedFile: event.target.files[0],
+      loaded: 0,
+    })
+  }
+
+  onClickHandler = () => {
+    const data = new FormData() 
+    data.append('image', this.state.selectedFile)
+    data.append('name', this.state.name)
+    data.append('desc', this.state.desc)
+    axios.post("http://localhost:5000/post", data, { 
+      // receive two    parameter endpoint url ,form data
+    })
+    .then (res => {
+      console.log(res.statusText)
+    })
+  }
+
+  render() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Upload to server</h1>
+      <div>
+        <div>
+            <label for="name">Image Title</label>
+            <input type="test" id="name" placeholder="Name" defaultValue="" name="name" required onChange={(e) => this.setState( { name: e.target.value })}>
+
+            </input>
+          </div>
+          <div>
+            <label for ="desc">Image Description</label>
+            <textarea id="desc" name="desc" defaultValue="" rows="2" placeholder="Description" required onChange={(e) => this.setState( { desc: e.target.value })}>
+
+            </textarea>
+          </div>
+          <div>
+          <label for="image">Upload Image</label>
+          <input type="file" id="image"
+            name="image" value="" required onChange={this.onChangeHandler}></input>
+          </div>
+          <div>
+            <button type="submit" onClick={this.onClickHandler}>Submit</button>
+          </div>
+      </div>
     </div>
   );
-}
+  }
+};
 
 export default App;
