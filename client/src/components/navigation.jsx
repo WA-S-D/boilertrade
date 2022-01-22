a2808f1b6a5059224621a11ac77d07e95a58e5a3
chi-wei-lien <lien2@purdue.edu>
2022-01-22 13:17:18 -0500

    show user name

--- client/src/components/navigation.jsx
+++ client/src/components/navigation.jsx
@@ -1,4 +1,44 @@
 export const Navigation = (props) => {
+  if (localStorage.getItem('user')) {
+    return (
+      <nav id='menu' className='navbar navbar-default navbar-fixed-top'>
+      <div className='container'>
+        <div className='navbar-header'>
+          <button
+            type='button'
+            className='navbar-toggle collapsed'
+            data-toggle='collapse'
+            data-target='#bs-example-navbar-collapse-1'
+          >
+            {' '}
+            <span className='sr-only'>Toggle navigation</span>{' '}
+            <span className='icon-bar'></span>{' '}
+            <span className='icon-bar'></span>{' '}
+            <span className='icon-bar'></span>{' '}
+          </button>
+
+            <a className='navbar-brand page-scroll' href='#page-scroll'>
+              Boiler Trade
+            </a>
+
+        </div>
+
+        <div
+          className='collapse navbar-collapse'
+          id='bs-example-navbar-collapse-1'
+        >
+          <ul className='nav navbar-nav navbar-right'>
+            <li>
+              <a href='/login' className='page-scroll'>
+              {localStorage.getItem('user')}
+              </a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+    )
+  }
   return (
     <nav id='menu' className='navbar navbar-default navbar-fixed-top'>
       <div className='container'>
