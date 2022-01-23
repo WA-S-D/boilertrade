fba60b94137e89eba8c37a44cf184a1121325606
Anthony Baumann <anthony.baumann@icloud.com>
2022-01-22 21:11:01 -0500

    Your Products Stuff

--- client/src/components/productDash.js
+++ client/src/components/productDash.js
@@ -12,12 +12,10 @@ let products = [
   { id: 2, name: 'Macbook', description: 'Apple macbook', image: 'https://images.pexels.com/photos/4260477/pexels-photo-4260477.jpeg?cs=srgb&dl=pexels-august-de-richelieu-4260477.jpg&fm=jpg'}
 ]
 
-
-
-class Dashboard extends Component {
+class productDash extends Component {
   constructor () {
     super()
-    this.state = { images: [] }
+    this.state = { yourProducts: [] }
   }
 
   componentDidMount() {
@@ -25,23 +23,27 @@ class Dashboard extends Component {
     withCredentials: true
   })
   .then((response) => {
-    this.setState({ images: response.data});
+    let images = response.data;
+
+    let prods = [];
+
+    for (let i = 0; i < images.length; i++) {
+      if (images[i].email == localStorage.getItem('user')) {
+        prods.push(images[i]);
+      }
+    }
+
+    this.setState({ yourProducts: prods });
   })
   }
-
-  // for (var i = 0; i < images.length; i++) {
-  //   if (!images.email.equals(localStorage.getItem('user'))) {
-
-  //   }
-  // }
 
   renderImages() {
     return (
       <div className='scrollable-block'>
         <Grid container justify="center" spacing={4}>
-          { this.state.images.map((image) => (
-            <Grid item key={image._id} xs={12} sm={6} md={2} lg={4}>
-              <Product product={image}></Product>
+          { this.state.yourProducts.map((yourProduct) => (
+            <Grid item key={yourProduct._id} xs={12} sm={6} md={2} lg={4}>
+              <Product product={yourProduct}></Product>
             </Grid>
           ))}
         </Grid>
@@ -51,18 +53,11 @@ class Dashboard extends Component {
   }
 
   render () {
-    const { images } = this.state
-    return images.length ? this.renderImages() : (
+    const { yourProducts } = this.state;
+    return yourProducts.length ? this.renderImages() : (
       <span>Loading Products...</span>
     )
   }  
 }
 
-  // for (var i = 0; i < images.length; i++) {
-  //   products[i].id = images._id;
-  //   products[i].description = images.desc;
-  //   products[i].name = images.name;
-  //   products[i].image = images.img;
-  // }
-
-export default Dashboard;
+export default productDash;
